ce28c128fd88d884c18f9afdc7cbea2fd23faa53
Kiryuanzu <anzu.nagato.1005@gmail.com>
2026-04-02 00:21:07 +0900

    Rewrite views with Tailwind classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,2 @@
 module ApplicationHelper
-  def calorie_tone_class(calories)
-    case calories
-    when 2501.. then "is-alert"
-    when 2001..2500 then "is-warning"
-    else "is-normal"
-    end
-  end
-
-  def meal_type_badge(meal_type)
-    "meal-#{meal_type.dasherize}"
-  end
 end
